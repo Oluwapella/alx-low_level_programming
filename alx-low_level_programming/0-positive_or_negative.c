0fe949b93f0800c0581d0391c2a669df62753839
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-02-19 13:04:24 -0800

    Positive anything is better than negative nothing

---
+++ alx-low_level_programming/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include  <stdlib.h>
+#include  <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+
+
+	printf("is positive\n");
+	else
+
+	printf("is positive\n");
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
