b44139528f918148e49fef2a4a306cbe003682e1
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-02-21 17:33:02 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	char *s = "_putchar\n";
+
+	while (*s)
+	putchar;
+	(*s++);
+	return (0);
+}
