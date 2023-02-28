999363c182f07d5ed109f67c68c7976940276559
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-02-28 15:08:19 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,15 +12,14 @@ void rev_string(char *s)
 	int c = 0;
 	int i;
 
-	while
-		(s[c]) != ('\0')
+	while (s[c] != '\0')
 
 		c++
-		for (i = 0; i < c; i++)
-		{
-			c--;
-			rv = s[i];
-			s[i] = s[c];
-			s[c] = rv;
-		}
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
 }
