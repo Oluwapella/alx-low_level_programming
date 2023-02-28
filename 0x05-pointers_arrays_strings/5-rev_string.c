04c9e5dbc818e4ce5443c536422b6b70d6d2ddf8
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-02-28 14:58:13 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ *@s: the string
+ * Return: its return reverses string
+ */
+
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while
+		(s[c]) != ('\0')
+
+		c++
+		for (i = 0; i < c; i++)
+		{
+			c--;
+			rv = s[i];
+			s[i] = s[c];
+			s[c] = rv;
+		}
+}
