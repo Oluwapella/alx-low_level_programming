32e2c084186aa332d6ac763e44fd9f7123a8d4dd
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-02-28 17:44:00 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string
+ *@str: the string
+ * Return:  prints half of a string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0
+
+	while (*st != '\0')
+	{
+		len++;
+		str++;
+	}
+	str - = (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
