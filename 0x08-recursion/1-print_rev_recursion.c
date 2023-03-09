b26990eba0b790cc697c1f9894d3e47a6980f45b
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-03-09 15:54:19 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#inclde "main.h"
+
+/**
+ *_print_rev_recursion - Write a function that prints a string in reverse.
+ *@s: a string
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+}
