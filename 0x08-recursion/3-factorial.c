2e7387cd52fa7060ba6693e3b497ffacd014a8d0
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-03-09 16:52:54 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - a function that prints a string, followed by a new line.
+ *@n: number to be  factoriaize
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return  (n * factorial``(n - 1));
+}
