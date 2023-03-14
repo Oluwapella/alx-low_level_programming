582001a9aa3b98d6debb5451da6bfe23b4ccf37f
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-03-13 17:33:32 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints its name, followed by a new line
+ *@argc:argument count
+ *@argv:argument vector
+ * Return: always (0)
+ */
+int main(int argc, char *argv[])
+{
+	int a ,b ,sum;
+	if (argc <= 2)
+	{
+	
+		printf("error\n");
+		return (1);
+}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a * b;
+	printf("%d\n", sum);
+return (0);
+}
